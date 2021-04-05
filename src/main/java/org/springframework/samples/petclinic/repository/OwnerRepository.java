c7df3e88202490021fe16bf4e773599b2a424137
davbrican <72500941+davbrican@users.noreply.github.com>
2021-04-05 19:00:10 +0200

    Update OwnerRepository.java

--- src/main/java/org/springframework/samples/petclinic/repository/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/OwnerRepository.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic.repository;
 import java.util.Collection;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
