60c082609f690a7995e2f1ed0f674b86e2337061
Maria <marcasbon@alum.es>
2021-04-16 10:42:10 +0200

    Fixes

--- src/main/java/org/springframework/samples/petclinic/model/Cause.java
+++ src/main/java/org/springframework/samples/petclinic/model/Cause.java
@@ -1,14 +1,25 @@
 package org.springframework.samples.petclinic.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
 
 import com.sun.istack.NotNull;
 
@@ -77,7 +88,26 @@ public class Cause extends BaseEntity{
 	public void setIsClosed(Boolean isClosed) {
 		this.isClosed = isClosed;
 	}
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "cause")
+	private Set<Donation> donations;
+	
+	protected Set<Donation> getDonationsInternal() {
+	    if (this.donations == null) {
+	       this.donations = new HashSet<>();
+	    }
+	    return this.donations;
+	}
 
+	public List<Donation> getDonations() {
+	    List<Donation> sortedDonations = new ArrayList<>(getDonationsInternal());
+	    PropertyComparator.sort(sortedDonations, new MutableSortDefinition("date", false, false));
+	    return Collections.unmodifiableList(sortedDonations);
+	}
+
+	public void addDonation(Donation donation) {
+	    getDonationsInternal().add(donation);
+	    donation.setCause(this);
+	}
 	
 
 }
